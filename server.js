cc58fd16114c38fa382d5641c26b1198d686d0f5
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2018-05-17 06:59:24 +0200

    170518

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors')
 
 var port = process.env.PORT || 3300;
 
+server.use(cors());
 server.use(jsonServer.bodyParser);
 
 // custom routes
 server.post("/login", function(req, res) {
-  if(req.body.login === "student" && req.body.password === "secret") {
+  if(req.body.username === "student" && req.body.password === "secret") {
   return res.json({
     name: "student",
     access_token: "3A364F68DA5745EB653457A585678"
@@ -18,17 +20,25 @@ server.post("/login", function(req, res) {
     
   }
 
-  if(req.body.login === "admin" && req.body.password === "bigsecret") {
+  if(req.body.username === "admin" && req.body.password === "bigsecret") {
   return res.json({
     name: "admin",
     access_token: "3A364F68DA5745EB653457A588765"
   });
 }
 
-res.sendStatus(401);
+res.status(401).json({
+  error: 'unknown username or password'
+});
 
 
 });
+
+server.use(
+  jsonServer.rewriter({
+    "/signup": "/users"
+  })
+);
 
 server.use(middlewares)
 server.use(router)
